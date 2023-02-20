af85ea63d0026523a73e00e7d6b678d60ff38311
Youngwoo Kim <ykim846@wisc.edu>
2023-02-20 06:21:20 -0600

    Changed Classroom.jsx and Student.jsx

--- src/components/Classroom.jsx
+++ src/components/Classroom.jsx
@@ -1,20 +1,93 @@
-import { Button, Container, Form, Row } from "react-bootstrap";
-
+import { useState, useEffect } from "react";
+import { Button, Container, Form, Row, Col } from "react-bootstrap";
+import Student from "./Student"
+ 
 const Classroom = () => {
+
+    const [backup, setBackup] = useState([]);
+    const [studs, setStuds] = useState([]);
+    
+    const[searchName, setSearchName] = useState("");
+    const[searchMajor, setSearchMajor] = useState("");
+    const[searchInterest, setSearchInterest] = useState("");
+
+    let nameIn = searchName.toUpperCase().trim();
+    let majorIn = searchMajor.toUpperCase().trim();
+    let interestIn = searchInterest.toUpperCase().trim();
+    console.log(nameIn, majorIn, interestIn);    
+    
+    
+    useEffect(() => {
+        const filtered = () => backup.filter((stud) => {
+            let full = stud.name.first + " " + stud.name.last;
+            return(
+                full.toUpperCase().includes(nameIn) &&
+                stud.major.toUpperCase().includes(majorIn) &&
+                stud.interests.some(i => i.toUpperCase().includes(interestIn))
+            );
+        })
+        setStuds(filtered);
+    }, [nameIn, majorIn, interestIn, backup])
+
+    useEffect(() => {
+        fetch('https://cs571.org/s23/hw4/api/students', {
+            method: "GET",
+            headers: {
+                "X-CS571-ID": "bid_c49825b5bd469d794555"
+            }
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            setBackup(data);
+            setStuds(data);
+        })
+        .catch(error => console.error(error));
+    }, [])
+    
+
+
     return <div>
         <Form>
             <Form.Label htmlFor="searchName">Name</Form.Label>
-            <Form.Control id="searchName"/>
+            <Form.Control 
+                id="searchName"
+                value = {searchName}
+                onChange={(e) => setSearchName(e.target.value)}
+            />
             <Form.Label htmlFor="searchMajor">Major</Form.Label>
-            <Form.Control id="searchMajor"/>
+            <Form.Control 
+                id="searchMajor"
+                value = {searchMajor}
+                onChange={(e) => setSearchMajor(e.target.value)}
+            />
             <Form.Label htmlFor="searchInterest">Interest</Form.Label>
-            <Form.Control id="searchInterest"/>
+            <Form.Control 
+                id="searchInterest"
+                value = {searchInterest}
+                onChange={(e) => setSearchInterest(e.target.value)}
+            />
             <br />
-            <Button variant="neutral">Reset Search</Button>
+            <Button 
+                variant="neutral"
+                onClick={(e) => {
+                    setStuds(backup);
+                    setSearchName("");
+                    setSearchMajor("");
+                    setSearchInterest("");
+                }}
+            >Reset Search</Button>
         </Form>
         <Container fluid>
             <Row>
-                { /* TODO Students go here! */ }
+                {
+                    studs.map(student => {
+                        return <Col xs={12} sm={6} md={4} lg={3} xl={2} key = {student.id}>
+                        <Student {...student}/>
+                        </Col>
+                    }
+                    )
+                }
             </Row>
         </Container>
     </div>
--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,7 +1,21 @@
+import { useState, useEffect } from "react";
+
 const Student = (props) => {
+
+    const[ints, setInts] = useState([]);
+
+    useEffect(() => {
+        setInts(props.interests);
+    }, [])
+    
     return <div>
         <h2>{props.name.first} {props.name.last}</h2>
-        {/* TODO Student data goes here! */}
+        <h4>{props.major}</h4>
+        <h4>{props.name.first} is taking {props.numCredits} credits and {props.fromWisconsin ? 'is':'is not'} from Wisconsin.</h4>
+        <h4>They have {props.interests.length} interests including...</h4>
+        <ul>{
+            ints.map(int => <li key = {props.id + int}>{int}</li>)
+        }</ul>
     </div>
 }
 
